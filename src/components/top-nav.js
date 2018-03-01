578967eb4a06d31db22e706365f772cf763641f0
Ali Ahmad <ahmad.aligohar@icloud.com>
2018-03-01 12:03:03 -0800

    Modal is working

--- src/components/top-nav.js
+++ src/components/top-nav.js
@@ -1,24 +1,41 @@
 import React from 'react';
-
+import InfoModal from './info-modal';
 import './top-nav.css';
 
-export default function TopNav(props) {
-    return (
-        <nav>
-            <ul className="clearfix">
-                <li>
-                    <a onClick={() => alert(` 1. I pick a random secret number between 1 to 100 and keep it hidden. \n 2. You need to guess until you can find the hidden secret number. \n 3. You will get feedback on how close ("hot") or far ("cold") your guess is.
-                    `)} className="what" href="#">
-                        What?
+export default class TopNav extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            overlay: false,
+        }
+    }
+
+    overlaySet() {
+        this.setState({
+            overlay: true,
+        })
+    }
+    render() {
+        return (
+            <div>
+                {this.state.overlay && <InfoModal />}
+                <nav>
+                    <ul className="clearfix">
+                        <li>
+                            <a onClick={() => this.overlaySet()} className="what" href="#">
+                                What?
                     </a>
-                </li>
-                <li>
-                    <a onClick={() => props.reset()} className="new" href="#">
-                        + New Game
+                        </li>
+                        <li>
+                            <a onClick={() => this.props.reset()} className="new" href="#">
+                                + New Game
                     </a>
-                </li>
-            </ul>
-        </nav>
-    );
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        );
+    }
+    
 }
 
