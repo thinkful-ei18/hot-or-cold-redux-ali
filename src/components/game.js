bb4a6a08350cd9c59286db82ee96b1b9786c3b1b
Ali Ahmad <ahmad.aligohar@icloud.com>
2018-03-01 11:10:12 -0800

    New game button resetting state

--- src/components/game.js
+++ src/components/game.js
@@ -14,12 +14,14 @@ export default class Game extends React.Component {
         }
     }
     
+/* ========== Add guess to state count ========== */
     userGuess(guess) {
         this.setState({
             count: [...this.state.count, guess]
         })
     }
-    
+
+/* ========== Display feedback to user based on current guess ========== */
     userFeedback(guess, randNum) {
         if (guess === undefined) {
             return 'Make a guess!'
@@ -31,11 +33,19 @@ export default class Game extends React.Component {
             return 'You win!!!';
         } 
     }
+
+/* ========== Reset state ========== */
+    userReset(){
+        this.setState({
+            count: [],
+            randNum: Math.floor(Math.random() * 100) + 1
+        })
+    }
     
     render() {
         return (
             <div>
-                <Header />
+                <Header reset={() => this.userReset()}/>
                 <GuessSection userGuess={(guess) => this.userGuess(guess)} 
                 feedback={() => this.userFeedback(this.state.count[this.state.count.length-1], this.state.randNum)}
                 />
